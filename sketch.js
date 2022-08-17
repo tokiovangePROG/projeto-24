d3eafe223acad32b04bf002f837fb0db8dec8ab7
otavio <107579773+tokiovangePROG@users.noreply.github.com>
2022-08-17 18:48:07 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,87 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+var angle=60;
+
+var ground, ground1, ground2, ground3;
+var b1,b2,b3,b4;
+var ball,rock;
+
+var btn1;
+var btn2;
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   
+  var ground_options ={
+    isStatic: true
+  };
+ 
+  var ball_options = {    
+    restitution: 0.75
+
+  }
+
+  var rock_options = {
+    restitution: 0.5,
+  }
+   
+  btn2 = createImg('up.png');
+  btn2.position(350,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(vForce);
+  
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  
+  rock = Bodies.circle(250,10,20,rock_options);
+  World.add(world,rock);
+
+  ground= Bodies.rectangle(200,390,400,20,ground_options);
+  ground1= Bodies.rectangle(10,200,20,400,ground_options);
+  ground2= Bodies.rectangle(390,200,20,400,ground_options);
+  ground3= Bodies.rectangle(200,0,400,20,ground_options);
+
+  World.add(world, ground);
+  World.add(world, ground1);
+  World.add(world, ground2);
+  World.add(world, ground3);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background("indigo");
+  Engine.update(engine);
+  strokeWeight(4);
+  fill("lightGreen")
+rect(ground.position.x,ground.position.y,400,20);
+ellipse(ball.position.x,ball.position.y,20);
+push();
+fill("gray");
+ellipse(rock.position.x,rock.position.y,20);
+pop();
+}
+
+function vForce()
+{
+Matter.Body.applyForce(ball,{x:0,y:0},{x:0.02,y:-0.005});
+
+Matter.Body.applyForce(rock,{x:0,y:0},{x:0.02,y:0.01});
+}
+
+
+  
+
+
